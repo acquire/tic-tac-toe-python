de9fe1734ade50089bed231ddef02a7f2be2a6e2
acquire <wegham@gmail.com>
2015-12-29 08:59:50 +1100

    Create check for draw functionality
    Create draw() method - checks for a draw match
    Create is_played() method - checks to see if square is used

--- ttt.py
+++ ttt.py
@@ -29,17 +29,37 @@ b[0] == b[4] == b[8] or \
 b[2] == b[4] == b[6] :
         return True
 
+def draw(board):
+    all_played = True #start by assuming it's all been played
+    for i in board:
+        all_played *= is_played(i) 
+    return all_played
+
+def is_played(char):
+    if type(char) is str:
+        return True
+    else:
+        return False      
+
 def mainloop():
     while(game_over == False):
         print_board(board)
+        
+        #check for win
         if(win(board)):
             print("Player {} wins!".format(current_player))
             exit()
         else:
             switch_player()
+        
+        #check for draw
+        if(draw(board)):
+            print("It's a draw!")
+            exit()
+        
+        #continue to get input
         print("Player {} turn:".format(current_player))
         position = int(input())
         board[position] = current_player
 
-#todo check for draw match
 mainloop()
