d88e454f63efc13185c54d2f7d0ac53338eed674
acquire <wegham@gmail.com>
2015-12-28 13:01:42 +1100

    rename file, functions and variables to conform to PEP-8
    benefit: code actually looks cleaner and more consistency

--- ttt.py
+++ ttt.py
@@ -1,16 +1,16 @@
 board = [0,1,2,3,4,5,6,7,8]
 players = ['x','o'] 
-currentPlayer = 'x'
+current_player = 'x'
 game_over = False
 
-def switchplayer():
-    global currentPlayer
-    index = players.index(currentPlayer)
+def switch_player():
+    global current_player
+    index = players.index(current_player)
     index = (index + 1) % 2 
-    currentPlayer = players[index] 
+    current_player = players[index] 
 
 #prints a board by taking the board list as parameter
-def printboard(b):
+def print_board(b):
     print(" {} | {} | {}".format(b[0],b[1],b[2]))
     print("===+===+===")
     print(" {} | {} | {}".format(b[3],b[4],b[5]))
@@ -19,10 +19,10 @@ def printboard(b):
 
 def mainloop():
     while(game_over == False):
-        printboard(board) 
-        print("Player {} turn:".format(currentPlayer))
+        print_board(board) 
+        print("Player {} turn:".format(current_player))
         position = int(input()) 
-        board[position] = currentPlayer 
-        switchplayer()
+        board[position] = current_player 
+        switch_player()
 
 mainloop()
