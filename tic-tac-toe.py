53e00bea2a0055df2d827ec4a9d764076b6d8b76
acquire <wegham@gmail.com>
2015-12-28 12:31:08 +1100

    dictionary players, curerntplayer, refactor switchplayer, wording input
    used list instead of dictionary for players
    create global value currentPlayer
    improve switchplayer function
    switchplayer now uses global currentPlayer
    change wording for input prompt
    change name of players from 0 and 1 to 'x' and 'o'
    use 'position' instead of 'pos' for less commenting

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,27 +1,28 @@
 board = [0,1,2,3,4,5,6,7,8]
-player = {0:'x',1:'O'} 
+players = ['x','o'] 
+currentPlayer = 'x'
 game_over = False
+
+def switchplayer():
+    global currentPlayer
+    index = players.index(currentPlayer)
+    index = (index + 1) % 2 
+    currentPlayer = players[index] 
 
 #prints a board by taking the board list as parameter
 def printboard(b):
-    print("{} | {} | {}".format(b[0],b[1],b[2]))
-    print("===+====+===")
-    print("{} | {} | {}".format(b[3],b[4],b[5]))
-    print("===+====+===")
-    print("{} | {} | {}".format(b[6],b[7],b[8]))
-
-def switchplayer(p):
-    p = (p + 1) % 2 
-    player = player[p] 
+    print(" {} | {} | {}".format(b[0],b[1],b[2]))
+    print("===+===+===")
+    print(" {} | {} | {}".format(b[3],b[4],b[5]))
+    print("===+===+===")
+    print(" {} | {} | {}".format(b[6],b[7],b[8]))
 
 def mainloop():
-    #let first player be x
-    p = 0
     while(game_over == False):
         printboard(board) 
-        print("Player {} position:".format(player))
-        pos = int(input()) #get position
-        board[pos] = player  
-        switchplayer(p)
+        print("Player {} turn:".format(currentPlayer))
+        position = int(input()) 
+        board[position] = currentPlayer 
+        switchplayer()
 
 mainloop()
